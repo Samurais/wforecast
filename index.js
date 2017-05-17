3835c9bea601e0a28bf1bfb09c2965fa2216d046
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-05-17 19:02:04 +0800

    Update API to x5

--- index.js
+++ index.js
@@ -4,27 +4,28 @@
 
 const request = require('superagent'),
     Q = require('q'),
-    log = require('debug')('wforecast');
+    log = require('debug')('wforecast'),
+    _ = require('lodash');
 
 
-const baseUrl = "https://api.heweather.com/x3";
+const baseUrl = "https://free-api.heweather.com/v5";
 
 
-var WForewast = function(apiKey){
-    if(!apiKey) throw new Error('Invalid token, get it from http://www.heweather.com/my/service');
+var WForewast = function (apiKey) {
+    if (!apiKey) throw new Error('Invalid token, get it from http://www.heweather.com/my/service');
     this.key = apiKey;
 }
 
 
-WForewast.prototype.getWeatherByCity = function(city){
+WForewast.prototype.getWeatherByCity = function (city) {
     let defer = Q.defer();
     request
-        .get(baseUrl+"/weather?city="+city+"&key="+this.key)
-        .set('Accept','application/json')
-        .end(function(err, res){
-            if(err) return defer.reject(err);
-            defer.resolve(res.body);
-        }); 
+        .get(baseUrl + "/weather?city=" + city + "&key=" + this.key)
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+            if (err) return defer.reject(err);
+            defer.resolve(res.body.HeWeather5[0].suggestion);
+        });
 
     return defer.promise;
 }
--- test/test.js
+++ test/test.js
@@ -7,15 +7,21 @@ const config = require('./config.json');
 
 test.cb('WForewast#getWeatherByCity', t => {
     var wf = require('../index.js')(config.apiKey);
+    try {
+        wf.getWeatherByCity('beijing')
+            .then(function (res) {
+                // console.log(JSON.stringify(res));
+                t.truthy(res.comf.txt)
+                console.log(res)
+                t.pass();
+                t.end();
+            }, function (err) {
+                console.error(err);
+                t.fail(err);
+                t.end();
+            });
+    } catch (e) {
+        t.fail(e)
+    }
 
-    wf.getWeatherByCity('beijing')
-        .then(function(res) {
-            console.log(JSON.stringify(res));
-            t.pass();
-            t.end();
-        }, function(err) {
-            console.error(err);
-            t.fail(err);
-            t.end();
-        });
 });
